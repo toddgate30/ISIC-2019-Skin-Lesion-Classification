ebd9bca4c65fcd75b44f6f80411d43578ce6085c
toddgate30 <toddgate30@gmail.com>
2026-07-24 10:21:06 -0600

    Implemented build_selector.py

--- src/selection_methods/build_selector.py
+++ src/selection_methods/build_selector.py
@@ -1,4 +1,17 @@
 import torch
+from math import floor
 
-def build_selector():
-    raise NotImplementedError
+def build_selector(config):
+    selection_method = config.get("selection_method", "Uniform")
+    if selection_method == "Uniform":
+        return uniform_selector
+    else:
+        raise NotImplementedError(f"Selection Method {selection_method} has not been implemented")
+
+def uniform_selector(metabatch_images, metabatch_labels, ratio):
+    metabatch_size = len(metabatch_labels)
+    batch_size = floor(batch_size * ratio)
+
+    selected_indices = torch.randperm(metabatch_size)[:batch_size]
+
+    return metabatch_images[selected_indices], metabatch_labels[selected_indices]
